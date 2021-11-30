170078e20af5122b0e1ce36acd019155923fec5a
rinaldi.fauzi <rinaldi.fauzi@pgn-solution.co.id>
2021-11-30 22:27:41 +0700

    learning on chapter 26 - flutter packages (http)

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,6 +10,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+     Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+     Map data = jsonDecode(response.body);
+     print(data);
+     print(data['title']);
+  }
+
+  @override
+  void initState() {//constructor
+    // TODO: implement initState
+    super.initState();
+    getData();
+    print('hey there!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
