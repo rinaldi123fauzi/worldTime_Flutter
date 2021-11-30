62de83fb0145d5d264a90a29eb63706de259851a
rinaldi.fauzi <rinaldi.fauzi@pgn-solution.co.id>
2021-11-30 21:55:41 +0700

    learning on chapter 25 - asynchronous code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -10,16 +10,35 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
 
+  void getData() async {
+    //simulate network request for a username
+    // await Future.delayed(Duration(seconds: 3), () {
+    //   print('rinaldi');
+    // });
+
+    //simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'rinaldi';
+    });
+
+    //simulate network request to get bio for the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'fauzi, engineer & alone';
+    });
+
+    print('$username - $bio');
+  }
+
   @override
   void initState() {//constructor
     // TODO: implement initState
     super.initState();
-    print('initState function run');
+    getData();
+    print('hey there!');
   }
 
   @override
   Widget build(BuildContext context) {
-    print('build function run');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
