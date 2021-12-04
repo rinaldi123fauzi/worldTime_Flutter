03062f304a75ba4d456e36f8eb1d78f22edbd65f
rinaldi.fauzi <rinaldi.fauzi@pgn-solution.co.id>
2021-12-04 19:16:44 +0700

    learning on chapter 31 - formatting & showing dates

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime{
   String location; //location name for the UI
@@ -28,7 +29,7 @@ class WorldTime{
       // print(now);
 
       //set time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     }catch(e){
       print('caught error : $e');
       time = 'could not get time data';
